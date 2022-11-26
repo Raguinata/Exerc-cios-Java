decef7d6f92adf39d9bbbb90e574f531be58f622
Leonardo Ravanelli <leonardo.lrc17@gmail.com>
2022-11-26 09:42:54 -0300

    Adicionar Int, Double & Float

---
+++ Int, Double & Float/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        float pontoFlutuante = 3.14f;
+
+        double salario = 1270.50;
+        int valor = (int) salario; // Transforma o a variável "salário" em int
+        System.out.println(valor);
+
+        double valor1 = 0.2;
+        double valor2 = 0.1;
+        double total = valor1 + valor2;
+
+        System.out.println(total);
+    }
+}
